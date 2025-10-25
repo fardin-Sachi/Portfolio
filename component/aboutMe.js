513c508364ddcbce1ef2c54b0a53aa66bab55c73
Fardin Ahsan <fardin2002@cseku.ac.bd>
2025-10-25 12:11:38 +0600

    Update: AboutMe text

--- component/aboutMe.js
+++ component/aboutMe.js
@@ -11,7 +11,12 @@ export default function AboutMe() {
       
       <div className='shadow-2xl rounded-2xl pb-6 sm:pb-8 md:pb-10'>
         <p className='text-justify font-[poppins] px-4 sm:px-8 md:px-12 lg:px-16 xl:px-28 text-base sm:text-lg md:text-xl lg:text-2xl py-4 sm:py-6 md:py-8'>
-          I am a Computer Science and Engineering graduate from Khulna University, specializing in modern web development technologies, including <b>React</b>, <b>Next.js</b>, <b>Express.js</b>, <b>Node.js</b>, <b>AngularJS</b>, and <b>Spring Boot</b>. My expertise lies in designing and implementing efficient, user-centric applications, underpinned by a strong foundation in academic research, particularly in blockchain technology.
+
+          I am a Computer Science and Engineering graduate from Khulna University, currently working as a <strong>Software Engineer</strong> at the <strong>ICT Center, Ahsanullah University of Science and Technology (AUST)</strong>. I specialize in developing scalable, high-performance applications using <strong>Angular</strong>, <strong>Spring Boot</strong>, and <strong>Oracle Database</strong>.
+          <br></br>
+          <br></br>
+          In addition to my professional stack, I have experience with <u><i>React</i></u>, <u><i>Next.js</i></u>, <u><i>Node.js</i></u>, <u><i>Express.js,</i></u> and <u><i>MongoDB</i></u>, which enables me to contribute across the full software development lifecycle. With a background in blockchain research and a strong focus on clean architecture and problem-solving, I am passionate about building secure, efficient, and user-centered software solutions.
+          <br></br>
           <br></br>
           Beyond technical skills, I have demonstrated exceptional leadership abilities through active roles in student organizations, where I honed my skills in collaboration, problem-solving, and project management. With a passion for continuous learning, I strive to stay updated with evolving technologies and apply my knowledge to make a meaningful impact in the tech industry.
         </p>
